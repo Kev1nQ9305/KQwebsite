0747f8cb1adc610b76fa2505609921c10e2aebc1
Kev1nQ9305 <134224442+Kev1nQ9305@users.noreply.github.com>
2023-05-22 15:03:55 +0000

    edoc

---
+++ javascript/home.js
@@ -0,0 +1,11 @@
+
+
+const typed = new Typed('.text-switch', {
+    strings: ['Aerospace Engineer', 'Student', 'Cal Poly Mustang'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    delaySpeed: 1000,
+    loop: true
+})
+
+
